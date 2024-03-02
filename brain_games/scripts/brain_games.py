8ddaae669175f82fbe6686724cbd03736a518de3
Anatoliy <anatoliy.klimanov@gmail.com>
2024-03-02 15:48:55 +0300

    qwe123

--- brain_games/scripts/brain_games.py
+++ brain_games/scripts/brain_games.py
@@ -1,9 +1,10 @@
 #!/usr/bin/env pithon3
 def hello():
-    print('Welcome to the Brain Games!')
+    print('Wel to the Brain Games!')
 
 def main():
     hello()
-
+    
 if __name__ == '__main__':
     main()
+
